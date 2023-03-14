1df4009cf591699bb6b66b34164bb182a3acf4c1
hanafi407 <menabung08@gmail.com>
2023-03-14 14:03:03 +0700

    add Hero

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -18,9 +18,12 @@ class ProductDetailScreen extends StatelessWidget {
             Container(
               width: double.infinity,
               height: 300,
-              child: Image.network(
-                loadedProduct.imageUrl!,
-                fit: BoxFit.cover,
+              child: Hero(
+                tag: loadedProduct.productId!,
+                child: Image.network(
+                  loadedProduct.imageUrl!,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -82,10 +82,13 @@ class ProductItem extends StatelessWidget {
             Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                 arguments: product.productId);
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder.png'),
-            image: NetworkImage(product.imageUrl!),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.productId!,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl!),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
